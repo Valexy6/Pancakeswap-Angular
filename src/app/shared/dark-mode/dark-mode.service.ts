5beda707d74f56f41e53690a17676482a370c387
Lucia Tagliareni <lucia.tagliareni@gmail.com>
2022-03-17 12:17:37 +0100

    initial commit

---
+++ src/app/shared/dark-mode/dark-mode.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DarkModeService {
+//da implementare con il local sotrage per tenere memoria della preferenza anche dopo il refresh o restart del browser
+  constructor() { }
+}
