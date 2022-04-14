5c6913a464bde009c78549bdc1992401d2642c92
Lucia Tagliareni <lucia.tagliareni@gmail.com>
2022-04-14 14:38:29 +0200

    add sortby

--- src/app/components/farms/table-filters/table-filters.component.ts
+++ src/app/components/farms/table-filters/table-filters.component.ts
@@ -6,13 +6,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./table-filters.component.css']
 })
 export class TableFiltersComponent implements OnInit {
-
   active = false;
-  activeLive=false;
+  activeLive = false;
+  options: string[] = [
+    "Hot",
+    "APR",
+    "Multiplier",
+    "Earned",
+    "Liquidity",
+  ]
+  optionSelected: string = "Hot";
+  optionsList!: string[];
+  isVisible: boolean = false
 
   constructor() { }
 
   ngOnInit(): void {
+    this.optionsList =  this.options.filter( item => item !== this.optionSelected);
+  }
+
+  selectOption(option: string) {
+    this.optionSelected = option;
+    this.optionsList =  this.options.filter( item => item !== this.optionSelected);
+    this.showOptions();
+  }
+
+  showOptions() {
+    this.isVisible = !this.isVisible;
   }
 
 }
