d720a2c5282529b55d6ed72642fb698db6a2d981
Valerio Ballarò <valerioballaro@MBP-de-Valerio.homenet.telecomitalia.it>
2022-04-15 12:57:51 +0200

    fix data

--- data/farms.data.ts
+++ data/farms.data.ts
@@ -6,7 +6,7 @@ export const FarmsData: IFarm[] = [
     imgUrl: 'assets/images/bunny-table-img.svg',
     earned: 0,
     apr: 38.06,
-    liquidity: 260250779,
+    liquidity: 260.250779,
     multiplier: 40,
     cakeEarned: 0,
     type: 'core'
@@ -16,7 +16,7 @@ export const FarmsData: IFarm[] = [
     imgUrl: 'assets/images/square-yellow-table-img.svg',
     earned: 0,
     apr: 17.83,
-    liquidity: 370348979,
+    liquidity: 370.348979,
     multiplier: 11,
     cakeEarned: 0,
     type: 'core'
@@ -25,8 +25,8 @@ export const FarmsData: IFarm[] = [
     name: 'Yelov-bnb',
     imgUrl: 'assets/images/helmet-table-img.svg',
     earned: 0,
-    apr: 255.16,
-    liquidity: 901863,
+    apr: 55.16,
+    liquidity: 901.863509,
     multiplier: 1,
     cakeEarned: 0,
     type: 'farm-auction'
@@ -37,7 +37,7 @@ export const FarmsData: IFarm[] = [
     imgUrl: 'assets/images/waves-table-img.svg',
     earned: 0,
     apr: 19.98,
-    liquidity: 1.393845,
+    liquidity: 1.39384565,
     multiplier: 0.1,
     cakeEarned: 0,
     type: 'farm-auction'
@@ -47,8 +47,8 @@ export const FarmsData: IFarm[] = [
     name: 'Pexis-bnb',
     imgUrl: 'assets/images/pear-table-img.svg',
     earned: 0,
-    apr: 198.16,
-    liquidity: 556.56,
+    apr: 98.16,
+    liquidity: 556.567432,
     multiplier: 0.5,
     cakeEarned: 0,
     type: 'core'
@@ -57,8 +57,8 @@ export const FarmsData: IFarm[] = [
     name: 'Gmire-bnb',
     imgUrl: 'assets/images/g-table-img.svg',
     earned: 0,
-    apr: 354.46,
-    liquidity: 969867,
+    apr: 54.46,
+    liquidity: 969.867432,
     multiplier: 1,
     cakeEarned: 0,
     type: 'core'
